674a06c0c4f7c45927b907327c601fa4851b5d78
h-s017 <pystar0417@gmail.com>
2026-06-26 00:37:51 +0800

    Add no-voucher status option

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hana-tax-book-v6';
+const CACHE_NAME = 'hana-tax-book-v7';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
 
 self.addEventListener('install', event => {

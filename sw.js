89eb8aa7d428bc9c2ba6bd2adce2ed78d16dbbfa
h-s017 <pystar0417@gmail.com>
2026-06-25 20:12:50 +0800

    Bump app cache for bookkeeping update

--- sw.js
+++ sw.js
@@ -1,14 +1,20 @@
-const CACHE_NAME = 'hana-tax-book-v1';
+const CACHE_NAME = 'hana-tax-book-v2';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
 });

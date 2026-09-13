c2cda0d1f00e78e17ee415a73900e6b30f2f7408
h-s017 <pystar0417@gmail.com>
2026-09-13 21:39:38 +0800

    Cache table tools script

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'hana-tax-book-sync-v29';
+const CACHE_NAME = 'hana-tax-book-sync-v30';
 const ASSETS = [
   './','./index.html','./styles.css','./manifest.webmanifest',
   './app.js','./monthly-report.js','./sync.js','./cashflow.js','./profitloss.js',
-  './js/screenshot-bookkeeping.js'
+  './js/screenshot-bookkeeping.js','./table-tools.js'
 ];
 
 self.addEventListener('install', event => {
